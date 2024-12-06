57217ab91f28b717e03a03f4ecd373aea4a6eae7
Johannes Kasimir <johanneskasimir@gmail.com>
2024-12-06 15:25:31 +0100

    docs: add docstrings and explanations

--- src/ess/amor/conversions.py
+++ src/ess/amor/conversions.py
@@ -18,6 +18,50 @@ from ..reflectometry.types import (
 
 
 def theta(wavelength, divergence_angle, L2, sample_rotation, detector_rotation):
+    '''
+    Angle of reflection.
+
+    Computes the angle between the scattering direction of
+    the neutron and the sample surface.
+
+    :math:`\\gamma^*` denotes the angle between the scattering direction
+    and the horizontal plane.
+    :math:`\\gamma` denotes the angle between the ray from sample position
+    to detection position
+    and the horizontal plane.
+    :math:`L_2` is the length of the ray from sample position to detector position.
+    :math:`v` is the velocity of the neutron at the sample.
+    :math:`t` is the travel time from sample to detector.
+
+    The parabolic trajectory of the neutron satisfies
+
+    .. math::
+
+        \\sin(\\gamma) L_2 = \\sin(\\gamma^*) v t - \\frac{g}{2} t^2
+
+    and
+
+    .. math::
+
+        \\cos(\\gamma) L_2 = \\cos(\\gamma^*) vt
+
+    where :math:`g` is the gravitational acceleration.
+
+    The second equation tells us that the approximation :math:`L_2=vt`
+    will have a small error if :math:`\\gamma` is close to 0 and
+    the difference between :math:`\\gamma` and :math:`\\gamma^*` is small.
+
+    Using this approximation we can solve the first equation,
+    and by expressing :math:`v` in terms of the wavelength we get
+
+    .. math::
+
+        \\sin(\\gamma^*) =
+        \\sin(\\gamma) + \\frac{g}{2} \\frac{L_2 \\lambda^2 h^2}{m_n^2}.
+
+    Finally, the scattering angle is obtained by subtracting the sample rotation
+    relative to the horizontal plane.
+    '''
     c = sc.constants.g * sc.constants.m_n**2 / sc.constants.h**2
     out = (c * L2 * wavelength**2).to(unit='dimensionless') + sc.sin(
         divergence_angle + detector_rotation
@@ -28,10 +72,20 @@ def theta(wavelength, divergence_angle, L2, sample_rotation, detector_rotation):
 
 
 def angle_of_divergence(theta, sample_rotation, angle_to_center_of_beam):
+    """
+    Difference between the incident angle and the center of the incident beam.
+    Useful for filtering parts of the beam that have too high divergence.
+
+    On the Amor instrument this is always in the interval [-0.75 deg, 0.75 deg],
+    but the divergence of the incident beam can be made lower.
+    """
     return (
         theta
         - sample_rotation
+        #
         - angle_to_center_of_beam
+        # "kappa" the natural incidence angle
+        # is a fixed parameter of the instrument
         - sc.scalar(0.245, unit='deg').to(unit='rad')
     )
 
@@ -39,6 +93,7 @@ def angle_of_divergence(theta, sample_rotation, angle_to_center_of_beam):
 def wavelength(
     event_time_offset, divergence_angle, L1, L2, chopper_phase, chopper_frequency
 ):
+    "Converts event_time_offset to wavelength using the chopper settings."
     out = event_time_offset.to(unit="ns", dtype="float64", copy=True)
     unit = out.bins.unit
     tau = (1 / (2 * chopper_frequency)).to(unit=unit)
@@ -48,6 +103,7 @@ def wavelength(
     frame_bound = tau - tof_offset
     maximum = 2 * tau - tof_offset
 
+    # Frame unwrapping
     out += sc.where(
         (minimum < event_time_offset) & (event_time_offset < frame_bound),
         tof_offset,
@@ -57,6 +113,8 @@ def wavelength(
             sc.scalar(np.nan, unit=unit),
         ),
     )
+    # Correction for path length through guides being different
+    # depending on incident angle.
     out -= (divergence_angle.to(unit="deg") / (180.0 * sc.units.deg)) * tau
     out *= (sc.constants.h / sc.constants.m_n) / (L1 + L2)
     return out.to(unit='angstrom', copy=False)
@@ -74,6 +132,7 @@ def add_common_coords_and_masks(
     wbins: WavelengthBins,
     beam_size: BeamSize[RunType],
 ) -> ReducibleData[RunType]:
+    "Adds coords and masks that are useful for both reference and sample measurements."
     da = da.transform_coords(
         ("wavelength", "theta", "angle_of_divergence", "Q"),
         {
