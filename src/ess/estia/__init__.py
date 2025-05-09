9394b48148d37de785314e0c5a7e4201d273cb8e
Jan-Lukas Wynen <jan-lukas.wynen@ess.eu>
2025-05-09 15:27:21 +0200

    Rename workflow to mcstas workflow

--- src/ess/estia/__init__.py
+++ src/ess/estia/__init__.py
@@ -2,26 +2,15 @@
 # Copyright (c) 2025 Scipp contributors (https://github.com/scipp)
 import importlib.metadata
 
-import sciline
-import scipp as sc
-import scippnexus as snx
-
-from ..reflectometry import providers as reflectometry_providers
 from ..reflectometry import supermirror
-from ..reflectometry.types import (
-    BeamDivergenceLimits,
-    BeamSize,
-    DetectorSpatialResolution,
-    NeXusDetectorName,
-    RunType,
-    Position,
-)
+
 from . import conversions, load, maskings, normalization, orso, resolution, workflow
 from .types import (
     AngularResolution,
     SampleSizeResolution,
     WavelengthResolution,
 )
+from .workflow import EstiaMcStasWorkflow
 
 try:
     __version__ = importlib.metadata.version(__package__ or __name__)
@@ -29,54 +18,13 @@ except importlib.metadata.PackageNotFoundError:
     __version__ = "0.0.0"
 
 
-providers = (
-    *reflectometry_providers,
-    *load.providers,
-    *conversions.providers,
-    *maskings.providers,
-    *workflow.providers,
-    *normalization.providers,
-    *orso.providers,
-)
-"""
-List of providers for setting up a Sciline pipeline.
-
-This provides a default Estia workflow including providers for loadings files.
-"""
-
-
-def default_parameters() -> dict:
-    return {
-        supermirror.MValue: sc.scalar(5, unit=sc.units.dimensionless),
-        supermirror.CriticalEdge: 0.022 * sc.Unit("1/angstrom"),
-        supermirror.Alpha: sc.scalar(0.25 / 0.088, unit=sc.units.angstrom),
-        BeamSize[RunType]: 2.0 * sc.units.mm,
-        DetectorSpatialResolution[RunType]: 0.0025 * sc.units.m,
-        Position[snx.NXsample, RunType]: sc.vector([0, 0, 0], unit="m"),
-        NeXusDetectorName: "detector",
-        BeamDivergenceLimits: (
-            sc.scalar(-0.75, unit='deg'),
-            sc.scalar(0.75, unit='deg'),
-        ),
-    }
-
-
-def EstiaWorkflow() -> sciline.Pipeline:
-    """
-    Workflow with default parameters for the Estia instrument.
-    """
-    return sciline.Pipeline(providers=providers, params=default_parameters())
-
-
 __all__ = [
     "AngularResolution",
-    "EstiaWorkflow",
+    "EstiaMcStasWorkflow",
     "SampleSizeResolution",
     "WavelengthResolution",
     "conversions",
-    "default_parameters",
     "load",
-    "providers",
     "resolution",
     "supermirror",
 ]
